2d59e913b6086d02915fe6a69e1accca4e743aa7
Thibaut Mottet <thibaut.mottet@gmai.com>
2019-07-22 16:45:26 +0200

    Fix

--- README.md
+++ README.md
@@ -1,46 +1,30 @@
-# homebridge-xiaomi-plant-monitor
+# homebridge-mi-led-desk-lamp
 
-This is a [Homebridge](https://github.com/nfarina/homebridge) plugin for exposing the Xiaomi Flower Care / Flower Mate / Flower Monitor / Mi Flora devices to HomeKit
+This is a [Homebridge](https://github.com/nfarina/homebridge) plugin for exposing the Xiaomi Led Desk Lamp to HomeKit
 
-![mi](image.jpeg)
+![mi](https://ae01.alicdn.com/kf/HTB1_8Uya7fb_uJkSne1q6zE4XXan/Original-Xiaomi-Mijia-LED-Desk-Lamp-Smart-Table-Lamps-Desklight-Support-Mobile-Phone-App-Control-4.jpg_640x640.jpg)
 
-## Prerequisite
+## Get your token and IP
 
-This plugin is using [node-mi-flora](https://github.com/demirhanaydin/node-mi-flora) / [Noble](https://github.com/noble/noble) in the background with the same package dependencies. You can install these dependencies using `apt-get`, if not already done.
+Good luck!!! (Because it's the hard part), some links to help you:
 
-```
-(sudo) apt-get install bluetooth bluez libbluetooth-dev libudev-dev
-```
-
-For more details and descriptions for other platforms see the [Noble documentation](https://github.com/noble/noble#readme). At the moment [Noble](https://www.npmjs.com/package/noble) does not support [Node 10](https://github.com/noble/node-bluetooth-hci-socket/issues/84). Please use Node 9 if you want to use this plugin, untis the issues with Noble are resolved.
-
-## Docker 
-
-This plugin work on docker I made a PR with a working configuration here: https://github.com/oznu/docker-homebridge/pull/211
-
-
-#### MAC address
-
-Ensure you know the MAC address of your Xiaomi Flower Care. You can use `hcitool lescan` to scan for devices. The device will appear as `AA:BB:CC:DD:EE:FF Flower care` in the list.
+* https://github.com/jghaanstra/com.xiaomi-miio/blob/master/docs/obtain_token.md
+* https://github.com/mediter/miio/blob/master/docs/ios-token-without-reset.md
 
 ### npm
 
 ```
-(sudo) npm install -g --unsafe-perm homebridge-xiaomi-plant-monitor
+npm install -g homebridge-mi-led-desk-lamp
 ```
-
-## Known Issues
-
 
 
 ## Example Configuration
 
 ```
 {
-    "accessory": "xiaomi-plant-monitor",
-    "name": "My plant name",
-    "macAddress": "C4:EC:0D:6A:6F:DE",
-    "scanDurationInMs": 60000,
-    "fetchDataIntervalInMs": 3600000
+    "accessory": "mi-led-desk-lamp",
+    "name": "My desk lamp",
+    "ip": "192.168.0.132",
+    "token": "6591d0ad2003dd32da75815f5d7def26"
 }
 ``` 
--- index.js
+++ index.js
@@ -1,113 +1,72 @@
-const miflora = require('miflora')
-var Service, Characteristic
-
+const miio = require('miio')
+let Service, Characteristic
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service
     Characteristic = homebridge.hap.Characteristic
 
-    homebridge.registerAccessory("homebridge-xiaomi-plant-monitor", "xiaomi-plant-monitor", MifloraAccessory)
+    homebridge.registerAccessory("homebridge-mi-led-desk-lamp", "mi-led-desk-lamp", MiLedDesklamp)
 }
 
-class MifloraAccessory {
+class MiLedDesklamp {
 
     constructor(log, config) {
         // Setup configuration
         this.log = log
-        this.name = config['name'] || 'Mi Plant'
-        if (!config['macAddress']) {
-            this.log('No mac address define for', this.name)
+        this.name = config['name'] || 'Mi desk lamp'
+        if (!config['ip']) {
+            this.log('No IP address define for', this.name)
             return
         }
-        this.macAddress = config['macAddress'].toLocaleLowerCase()
-        this.scanDurationInMs = config['scanDurationInMs'] || 60000
-        this.fetchDataIntervalInMs = config['fetchDataIntervalInMs'] || 3600000
-
-        // Data
-        this.currentFirmwareVersion = '0.0.0'
-        this.currentBattery = 0
-        this.isLowBattery = this.currentBattery < 10
-        this.currentTemperature = 0
-        this.currentMoisture = 0
-        this.currentLux = 0
-        this.currentFertility = 0
-        this.isActive = false
+        if (!config['token']) {
+            this.log('No token define for', this.name)
+            return
+        }
+        this.ip = config['ip']
+        this.token = config['token']
 
         // Setup services
-        this.informationService = new Service.AccessoryInformation()
-        this.informationService
-            .setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
-            .setCharacteristic(Characteristic.Model, 'Plant Monitor')
-            .setCharacteristic(Characteristic.SerialNumber, this.macAddress)
-        this.informationService.getCharacteristic(Characteristic.FirmwareRevision)
-            .on('get', callback => callback(null, this.currentFirmwareVersion))
-
-        this.batteryService = new Service.BatteryService(this.name)
-        this.batteryService.setCharacteristic(Characteristic.ChargingState, Characteristic.ChargingState.NOT_CHARGEABLE)
-        this.batteryService.getCharacteristic(Characteristic.BatteryLevel)
-            .on('get', callback => callback(null, this.currentBattery))
-        this.batteryService.getCharacteristic(Characteristic.StatusLowBattery)
-            .on('get', callback => callback(null, this.getLowBatteryCharacteristic()))
-
-        this.humidityService = new Service.HumiditySensor(this.name)
-        this.humidityService.getCharacteristic(Characteristic.CurrentRelativeHumidity)
-            .on('get', callback => callback(null, this.currentMoisture))
-        this.humidityService.getCharacteristic(Characteristic.StatusLowBattery)
-            .on('get', callback => callback(null, this.getLowBatteryCharacteristic()))
-        this.humidityService.getCharacteristic(Characteristic.StatusActive)
-            .on('get', callback => callback(null, this.isActive))
-
-        // Fetch sensor data
-        this.fetchDataSensor().catch(error => this.log(error))
+        this.switch = new Service.Switch(this.name)
+        this.switch.getCharacteristic(Characteristic.On)
+            .on('get', this.getState)
+            .on('set', this.setState)
     }
 
-    async fetchDataSensor() {
-        this.log('Scanning %s for a max of %s seconds', this.macAddress, this.scanDurationInMs / 1000)
-        this.log('Fetch data every %s seconds', this.fetchDataIntervalInMs / 1000)
-        const devices = await miflora.discover({
-            addresses: [this.macAddress],
-            ignoreUnknown: true,
-            duration: this.scanDurationInMs
-        })
-        const device = devices.find(entry => entry.address === this.macAddress)
-        if (device) {
-            await this.getPlantData(device)
-            setInterval(() => this.getPlantData(device), this.fetchDataIntervalInMs)
-        } else {
-            this.log('Device %s not found', this.macAddress)
-        }
+    async setup() {
+
+
+        // Setup services
+        this.switch = new Service.Switch(this.name)
+        this.switch.getCharacteristic(Characteristic.On)
+            .on('get', this.getState)
+            .on('set', this.setState)
     }
 
-    async getPlantData(device) {
+    async getState(callback) {
+        this.log('Get state...')
         try {
-            const {firmwareInfo: {battery, firmware}, sensorValues: {temperature, lux, moisture, fertility}} = await device.query()
-            this.currentFirmwareVersion = firmware
-            this.currentBattery = battery
-            this.currentTemperature = temperature
-            this.currentLux = lux
-            this.currentMoisture = moisture
-            this.currentFertility = fertility
-            this.isActive = true
-            this.log(`battery: ${this.currentBattery}%  firmware: ${this.currentFirmwareVersion} temperature: ${this.currentTemperature}° lux: ${this.currentLux} moisture: ${this.currentMoisture}% fertility: ${this.currentFertility}`)
-
-            this.informationService.getCharacteristic(Characteristic.FirmwareRevision).updateValue(this.currentFirmwareVersion)
-
-            this.humidityService.getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(this.currentMoisture)
-            this.humidityService.getCharacteristic(Characteristic.StatusLowBattery).updateValue(this.getLowBatteryCharacteristic())
-            this.humidityService.getCharacteristic(Characteristic.StatusActive).updateValue(this.isActive)
-
-            this.batteryService.getCharacteristic(Characteristic.BatteryLevel).updateValue(this.currentBattery)
-            this.batteryService.getCharacteristic(Characteristic.StatusLowBattery).updateValue(this.getLowBatteryCharacteristic())
+            const device = await miio.device({address: this.ip, token: this.token})
+            const power = await device.power()
+            callback(null, power)
         } catch (e) {
-            this.log(e)
+            this.log.error('Error getting state', e)
+            callback(e)
         }
     }
 
-    getLowBatteryCharacteristic() {
-        return this.isLowBattery ? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL
+    async setState(state, callback) {
+        this.log('Set state to', state)
+        try {
+            const device = await miio.device({address: this.ip, token: this.token})
+            await device.power(state)
+            callback(null)
+        } catch (e) {
+            this.log.error('Error setting state', e)
+            callback(e)
+        }
     }
 
     getServices() {
-        return [this.informationService, this.batteryService, this.humidityService]
+        return [this.switch]
     }
 }
